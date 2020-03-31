a965602b42a0025c2e40f83487b6013632ac455b
Magdalena <miang@gazeta.pl>
2020-03-31 20:35:53 +0200

    bleeee cos tam

--- src/Controller/DomyslnyController.php
+++ src/Controller/DomyslnyController.php
@@ -1,1 +1,22 @@
 <?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DomyslnyController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+
+
+        return new Response(
+            '<html><body>cos tam</body></html>'
+        );
+    }
+}
+
